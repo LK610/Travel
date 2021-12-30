74173dfad7616532826ee1cf66815ffc39c2c17f
LK610 <96885717+LK610@users.noreply.github.com>
2021-12-30 13:46:41 -0500

    Create Travel_Module.py

---
+++ Travel_Module.py
@@ -0,0 +1,10 @@
+################################################################################
+##
+## Filename: Travel_Module.py
+## Purpose: Defines frequently used functions for the travel project
+## Author: Laura Kaufmann
+##
+################################################################################
+
+def getGDBlocation():
+  return r"C:\Users\Laura\Documents\Academia\Penn State University\GEOG 485 Python Programming\Final Prj\Final Project"
