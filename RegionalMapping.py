220c0e924ca39dbcb839a6125f82e85c32deee1d
LK610 <Laura.Kaufmann.610@gmail.com>
2021-12-30 14:30:17 -0500

    Update RegionalMapping.py

--- RegionalMapping.py
+++ RegionalMapping.py
@@ -5,3 +5,18 @@
 ## Author: Laura Kaufmann
 ##
 ################################################################################
+
+# Import and connect
+
+## Import packages
+import arcpy
+import os
+import datetime as dt
+import Travel_Module
+
+## Define workspaces
+db_location = Travel_Module.getGDBlocation
+arcpy.env.overwriteOutput = True
+arcpy.env.workspace = db_location
+
+print(db_location)
