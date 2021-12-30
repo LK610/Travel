33bccd9b376544d79aaba485849895c22620530a
LK610 <Laura.Kaufmann.610@gmail.com>
2021-12-30 15:21:48 -0500

    Update Travel_Module.py

--- Travel_Module.py
+++ Travel_Module.py
@@ -6,10 +6,25 @@
 ##
 ################################################################################
 
-# Defines geodatabase location; to be used in all scripts
-def getGDBlocation():
-  return r"C:\Users\Laura\Documents\Academia\Penn State University\GEOG 485 Python Programming\Final Prj\Final Project"
+# Connecting to the 
+## Defines geodatabase location; to be used in all scripts
+def getGDBconnection():
+  import os
+  db_loc = r"C:\Users\Laura\AppData\Roaming\Esri\ArcGISPro\Favorites\Travel_PROD.sde"
+  db_name = r"Travel_PROD.DBO"
+  db_path = os.path.join(db_loc, db_name)
+  return [db_loc, db_name, db_path]
+
+# Working with time stamps
+## Get today's date
+def getTodayDate():
+  import datetime as dt
+  return dt.datetime.now()
+
+## Format an input date for inclusion in file names
+def formatFileDate(in_date):
+  return in_date.strftime("%Y_%m_%d")
 
 # ???
-def compare_geometry():
-  return "IDK"
+#def compare_geometry():
+#  return "IDK"
