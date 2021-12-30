c3bc573b540302b5c750f42f376f4e46d285b4c9
LK610 <Laura.Kaufmann.610@gmail.com>
2021-12-30 14:19:30 -0500

    Update Travel_Module.py

--- Travel_Module.py
+++ Travel_Module.py
@@ -6,5 +6,9 @@
 ##
 ################################################################################
 
+# Defines geodatabase location; to be used in all scripts
 def getGDBlocation():
   return r"C:\Users\Laura\Documents\Academia\Penn State University\GEOG 485 Python Programming\Final Prj\Final Project"
+
+def compare_geometry():
+  return "IDK"
