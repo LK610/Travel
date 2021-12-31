e5a081dcb7c63e177f73bf3722cc4d415fa1d5ed
LK610 <Laura.Kaufmann.610@gmail.com>
2021-12-30 19:58:37 -0500

    Update RegionalMapping.py

--- RegionalMapping.py
+++ RegionalMapping.py
@@ -25,9 +25,9 @@ file_name = str("Trip_Overview_" + file_date + ".pdf")
 file_path = os.path.join(scratchWorkspace, file_name)
 
 ## Connect to database
-regions = db_connect[2] + "\Regions"
-trips = db_connect[2] + "\Trips"
-worldhex = db_connect[2] + "\WorldHex"
+regions = db_connect[2] + ".Regions"
+trips = db_connect[2] + ".Trips"
+worldhex = db_connect[2] + ".WorldHex15000"
 
 euro_poly_fc_name = scratchWorkspace + "\Dissolved_Trips"
 
@@ -36,15 +36,18 @@ euro_poly_fc_name = scratchWorkspace + "\Dissolved_Trips"
 print("Workspaces and variables defined...")
 
 # Create layer of dissolved hexagons
+def euro_trip_shapes():
+    try:
+        ## Select features where Trip_ID <> "000"
+        hex_europe = arcpy.management.SelectLayerByAttribute(worldhex, "NEW_SELECTION", "Trip_ID <> '000'", None)
 
-try:
-    ## Select features where Trip_ID <> "000"
-    hex_europe = arcpy.management.SelectLayerByAttribute(worldhex, "NEW_SELECTION", "Trip_ID <> '000'", None)
+        ## Generate merged shapes based on the Trip_ID
+        dissolved_trips = arcpy.analysis.PairwiseDissolve(hex_europe, euro_poly_fc_name, ["Trip_ID"], "", "MULTI_PART")
+    
+        arcpy.AddMessage("WorldHex features dissolved...")
+    
+    except:
+        arcpy.AddMessage("Something went wrong dissolving WorldHex features. Please check the script.")
 
-    ## Generate merged shapes based on the Trip_ID
-    dissolved_trips = arcpy.analysis.PairwiseDissolve(hex_europe, euro_poly_fc_name, ["Trip_ID"], "", "MULTI_PART")
-    
-    arcpy.AddMessage("WorldHex features dissolved...")
-    
-except:
-    arcpy.AddMessage("Something went wrong dissolving WorldHex features. Please check the script.")
+
+#euro_trip_shapes()
