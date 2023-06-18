7e3f36411cae799bd261f9bf0c5bb0c4312e2e7b
LK610 <Laura.Kaufmann.610@gmail.com>
2023-06-18 17:58:12 -0400

    Create geodatabase from Excel template

---
+++ GDBfromSchema.py
@@ -0,0 +1,30 @@
+################################################################################
+## Filename: GDBfromSchema.py
+## Purpose: Create an empty file geodatabase from an Excel template
+## Author: Laura Kaufmann
+################################################################################
+
+# IMPORT PACKAGES
+import arcpy
+import os
+import pandas as pd
+
+# ABOUT THE TARGET GEODATABASE
+sr = arcpy.SpatialReference(4326)
+
+fldr = r"C:\Users\Laura\Documents\Keepsakes\Travel\0_MetadataInstructions\2022 Database Migration"
+gdb = r"Travel_Archive.gdb"
+wrkspc = os.path.join(fldr, gdb)
+
+arcpy.env.overwriteOutput = True
+
+# READ IN THE TEMPLATE SPREADSHEET
+xlsx_fldr = r"C:\Users\Laura\Documents\Keepsakes\Travel\0_MetadataInstructions"
+xlsx_file = r"Data_Dictionary.xlsx"
+xlsx = os.path.join(xlsx_fldr, xlsx_file)
+
+tables = pd.read_excel(xlsx, sheet_name='Tables')
+print(tables)
+
+#arcpy.management.CreateFeatureclass(wrkspc, out_name, {geometry_type}, None, {has_m}, {has_z}, sr)
+#arcpy.management.CreateTable(wrkspc, out_name, None, '', '')
