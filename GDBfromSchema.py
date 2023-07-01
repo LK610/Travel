f35b1da81c7912f3b04a4b54d069a8316ba2a1a5
LK610 <Laura.Kaufmann.610@gmail.com>
2023-07-01 19:11:02 -0400

    Create domains

--- GDBfromSchema.py
+++ GDBfromSchema.py
@@ -6,22 +6,35 @@
 
 # IMPORT PACKAGES
 import arcpy
+
 import os
 import pandas as pd
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 # ABOUT THE TARGET GEODATABASE
 sr = arcpy.SpatialReference(4326)
 
 fldr = r"C:\Users\Laura\Documents\Keepsakes\Travel\0_MetadataInstructions\2022 Database Migration"
-gdb = r"Travel_Archive.gdb"
-wrkspc = os.path.join(fldr, gdb)
-
-arcpy.env.overwriteOutput = True
+name = r"Travel_Archive"
+gdb = name + ".gdb"
 
 # READ IN THE TEMPLATE SPREADSHEET
 xlsx_fldr = r"C:\Users\Laura\Documents\Keepsakes\Travel\0_MetadataInstructions"
 xlsx_file = r"Data_Dictionary.xlsx"
 xlsx = os.path.join(xlsx_fldr, xlsx_file)
+
+logging.info('Packages imported; ready to begin')
+
+# DELETE AND CREATE THE TARGET GEODATABASE
+arcpy.management.Delete(os.path.join(fldr, gdb), '')
+arcpy.management.CreateFileGDB(fldr, name, "CURRENT")
+
+wrkspc = os.path.join(fldr, gdb)
+arcpy.env.overwriteOutput = True
+
+logging.info('Blank file geodatabase created')
 
 # CREATE TABLES
 tables = pd.read_excel(xlsx, sheet_name='Tables')
@@ -35,8 +48,10 @@ for index, row in tables.iterrows():
 
     if geometry_type == 'TABLE':
         arcpy.management.CreateTable(wrkspc, out_name, None, '', '')
+        logging.info('%s table created in the geodatabase', out_name)
     else:
         arcpy.management.CreateFeatureclass(wrkspc, out_name, geometry_type, None, has_m, has_z, sr)
+        logging.info('%s feature class created in the geodatabase', out_name)
 
 # CREATE DOMAINS
 domains = pd.read_excel(xlsx, sheet_name='Domains')
@@ -51,7 +66,4 @@ for index, row in domains.iterrows():
     merge_policy = row['MergePolicy']
 
     arcpy.management.CreateDomain(wrkspc, domain_name, domain_description, field_type, domain_type, split_policy, merge_policy)
-
-# CREATE DOMAIN VALUES
-
-#arcpy.management.AddCodedValueToDomain(in_workspace, domain_name, code, code_description)
+    logging.info('%s domain created in the geodatabase', domain_name)
