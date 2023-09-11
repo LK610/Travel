89472e6768fe4783b51c859f24d352e7c3db8b1b
LK610 <Laura.Kaufmann.610@gmail.com>
2023-09-10 22:44:57 -0400

    Added tables, fields, metadata

--- GDBfromSchema.py
+++ GDBfromSchema.py
@@ -6,6 +6,7 @@
 
 # IMPORT PACKAGES
 import arcpy
+from arcpy import metadata as md
 
 import os
 import pandas as pd
@@ -25,8 +26,19 @@ xlsx_fldr = r"C:\Users\Laura\Documents\Keepsakes\Travel\0_MetadataInstructions"
 xlsx_file = r"Data_Dictionary.xlsx"
 xlsx = os.path.join(xlsx_fldr, xlsx_file)
 
+# METADATA VARIABLES
+credits = "Schema designed and data populated by Laura Kaufmann (lmmk81914@gmail.com)"
+constraints = "Data and schema can only be used with writted permission from Laura Kaufmann (lmmk81914@gmail.com)"
+
 # FOLDER OF SQL TXT FILES FOR VIEWS
 sqlFldr = r"C:\Users\Laura\Documents\Keepsakes\Travel\0_MetadataInstructions\ViewSQL"
+
+# FUNCTIONS
+def getValue(argument):
+    if argument == 'NONE':
+        return None
+    else:
+        return argument
 
 logging.info('Packages imported; ready to begin')
 
@@ -66,23 +78,81 @@ for index, row in domains.iterrows():
     logging.info('%s domain and values added to the geodatabase', domain_name)
 
 # CREATE TABLES
-## Create feature class: https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/create-feature-class.htm
 tables = pd.read_excel(xlsx, sheet_name='Tables')
-print(tables)
+tables = tables.fillna('NONE')
 
-"""for index, row in tables.iterrows():
+fields = pd.read_excel(xlsx, sheet_name='Fields')
+fields = fields.fillna('NONE')
 
+for index, row in tables.iterrows():
+    
     out_name = row['Name']
     geometry_type = row['Geometry']
     has_m = row['HasM']
     has_z = row['HasZ']
-
+    summary = row['TableDefinition']
+    
+    if row['Module'] == 'None':
+        tag = geometry_type.capitalize()
+    else:
+        tag = "{}, {}".format(row['Module'], geometry_type.capitalize())
+        
     if geometry_type == 'TABLE':
+        ##https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/create-table.htm
         arcpy.management.CreateTable(wrkspc, out_name, None, '', '')
         logging.info('%s table created in the geodatabase', out_name)
     else:
+        ##https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/create-feature-class.htm
         arcpy.management.CreateFeatureclass(wrkspc, out_name, geometry_type, None, has_m, has_z, sr)
         logging.info('%s feature class created in the geodatabase', out_name)
     
-    logging.info('%s table created in the geodatabase', out_name)"""
-
+    mdDesc = []
+    
+    for index, row in fields.iterrows():
+        if row['Table'] == out_name:
+            
+            field_name = getValue(row['FieldName'])
+            field_type = getValue(row['FieldType'])
+            field_precision = getValue(row['Precision'])
+            field_scale = getValue(row['Scale'])
+            field_length = getValue(row['Length'])
+            field_alias = getValue(row['FieldAlias'])
+            field_is_nullable = getValue(row['Nullable'])
+            field_is_required = getValue(row['Required'])
+            field_domain = getValue(row['FieldDomain'])
+            field_default = getValue(row['DefaultValue'])
+            
+            ##https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/add-field.htm
+            arcpy.management.AddField(os.path.join(wrkspc, out_name), field_name, field_type, field_precision, field_scale, field_length, field_alias, field_is_nullable, field_is_required, field_domain)
+            
+            if field_default != None:
+                ##https://pro.arcgis.com/en/pro-app/latest/tool-reference/data-management/assign-default-to-field.htm
+                arcpy.management.AssignDefaultToField(os.path.join(wrkspc, out_name), field_name, field_default)
+                setDesc = " (Default: {})".format(field_default)
+            
+            if field_domain != None:
+                setDesc = " ({})".format(field_domain)
+            
+            if field_default != None and field_domain != None:
+                setDesc = " (Default: {} ({}))".format(field_default, field_domain)
+            else:
+                setDesc = ""
+            
+            fieldDesc = "{} ({}) - {}{}".format(field_name, field_type.capitalize(), row['FieldDefinition'], setDesc)
+            mdDesc.append(fieldDesc)
+            
+    logging.info('Fields added to %s', out_name)
+    
+    ##https://pro.arcgis.com/en/pro-app/latest/arcpy/metadata/metadata-class.htm
+    new_md = md.Metadata()
+    new_md.title = out_name
+    new_md.tags = tag
+    new_md.summary = summary
+    new_md.description = '\n'.join(mdDesc)
+    new_md.credits = credits
+    new_md.accessConstraints = constraints
+    
+    tgt_item_md = md.Metadata(os.path.join(wrkspc, out_name))
+    if not tgt_item_md.isReadOnly:
+        tgt_item_md.copy(new_md)
+        tgt_item_md.save()
